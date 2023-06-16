f40007301f523f4d969ee8880bfc94b2861e97b4
k-miyazoe <nk.miyazoe@gmail.com>
2023-06-17 07:04:35 +0900

    サイドバーコンポーネントのupdate

--- react_ts_app/react-app/src/App.tsx
+++ react_ts_app/react-app/src/App.tsx
@@ -1,22 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Todo from "./components/Todo/Todo";
-import Sidebar from "./components/Sidebar/Sidebar";
+import Wallet from "./components/Wallet/Wallet";
 
 function App() {
   const profile = {
-    name: 'John Doe',
-    walletaddress: '0x7d632a8d43B1cF0bdd4a66839CeC187eD45C146D',
-    bio: 'Hello, I am John Doe.',
+    name: 'none',
+    walletaddress: '0x7d632a8d43B1cF0bdd4a66839CeC187eD45C146D'
   };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h2>Todo List</h2>
         <Todo />
-        <Sidebar profile={profile} />
+        <Wallet profile={profile} />
       </header>
     </div>
   );
